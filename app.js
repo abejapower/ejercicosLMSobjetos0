4ded1adc427127db6bf689b5b5c286f271a4094c
Nicole Elgueta <nicole.m.elgueta@gmail.com>
2018-06-26 17:12:06 +0200

    haciendo ejercicios del lms por recomendacion de marcia

---
+++ app.js
@@ -0,0 +1,30 @@
+const favoriteMovies = [
+  'fight club',
+  'kill bill',
+  'the matrix',
+  'the godfather',
+  ];
+  //console.log(favoritesMovies);
+  console.log(favoriteMovies[2])
+
+let movie = {
+     title:'fight club',
+     director:' David Fincher',
+     protagonists:['Edward Norton','Brad Pitt'],
+     release:"1999",
+   };
+   //console.log(movie)
+   console.log(movie.protagonists[1])
+const person = {
+  name: 'nicole',
+  age: '36',
+  classmates: ['carolaA','caroalaL', 'eli','isa','paola'],
+};
+//console.log(person)
+console.log(person.classmates[3])
+  
+
+
+
+  
+  
